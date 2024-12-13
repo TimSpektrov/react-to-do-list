16e4ea98b979a567d487800fc39235af6b65330c
Tim Spektrov <tspektrov@mail.ru>
2024-12-13 20:57:54 +0300

    hotfix

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -3,7 +3,7 @@ import styles from './filters.module.scss'
 import {useTodo} from "../../hooks/useTodoList.tsx";
 import cn from 'classnames'
 interface IFiltersProps {}
-export const Filters: FC<IFiltersProps> = (props) => {
+export const Filters: FC<IFiltersProps> = () => {
   const { filters, filterTodo, selectedFilter } = useTodo()
   return (
     <div className={styles.list}>
